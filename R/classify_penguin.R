8776f318cb644e25460567542d9096c4ab4e3e71
Vera Danilova <veradanilova765@gmail.com>
2023-03-28 10:14:42 -0700

    fixed build, removed testthat, licnese update

--- R/classify_penguin.R
+++ R/classify_penguin.R
@@ -1,4 +1,4 @@
-library(testthat)
+
 classify_penguin <- function(bill_length, flipper_length){
   if (flipper_length > 205){
     return ("Gentoo")
@@ -11,7 +11,4 @@ classify_penguin <- function(bill_length, flipper_length){
   }
 }
 
-expect_equal(classify_penguin(40,210), "Gentoo")
-expect_equal(classify_penguin(40,200), "Adelle")
-stopifnot(classify_penguin(40,210) == "Gentoo") #another way to check for error
 
